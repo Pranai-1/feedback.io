1d16ef37955087aee804e9ecc11b148934ba821a
Pranai-1 <reddypranai2017@gmail.com>
2024-11-29 12:59:35 +0530

    completed responsiveness

--- app/components/SpaceCreationHome.tsx
+++ app/components/SpaceCreationHome.tsx
@@ -8,7 +8,7 @@ import ShineBorder from "@/components/ui/shine-border";
 export default function CreateSpace({displayPage,setDisplayPage}:{displayPage:number,setDisplayPage:React.Dispatch<React.SetStateAction<number>>}){
   
     return(
-        <ShineBorder className="bg-[#FFFFFF] md:w-[620px] sm:px-4 sm:p-4 w-[95%] rounded-lg">
+        <ShineBorder className="bg-[#FFFFFF] md:w-[720px] sm:px-4 sm:p-4 w-[95%] rounded-lg">
        <FormNavigator setDisplayPage={setDisplayPage} displayPage={displayPage}/>
        {displayPage==0 ? (
         <SpaceForm/>
--- app/components/spaceComponenets/SpaceCreationHome.tsx
+++ app/components/spaceComponenets/SpaceCreationHome.tsx
@@ -7,17 +7,17 @@ export default function SpaceCreationHome({setCreateSpaceToggle}:{setCreateSpace
     const[displayPage,setDisplayPage]=useState(0)
     return(
         <>
-          <div className=" absolute top-4 sm:top-0 left-0 w-full z-50 flex justify-center items-center ">
+          <div className=" absolute top-4 sm:top-0 left-0 w-full  z-50 flex justify-center items-center ">
         <div className="bg-gray-50 rounded-md shadow-lg flex justify-center w-[95%]">
          
           <button
-            className="absolute top-2 sm:top-6 right-10 text-gray-500 hover:text-black text-lg"
+            className="absolute top-2 sm:top-6 right-4 sm:right-20 text-gray-500 hover:text-black text-lg"
             onClick={() => setCreateSpaceToggle(false)}
           >
             &#10005;
           </button>
         
-          <div className="w-[100%] flex flex-wrap justify-center items-center  lg:justify-start lg:items-start gap-4 mb-10 pt-4">
+          <div className="w-[100%] flex flex-wrap justify-center items-center  md:items-start gap-8  md:gap-20 mb-10  py-10">
             <SpaceCreationProvider>
             <LivePreview displayPage={displayPage} />
             <CreateSpace displayPage={displayPage} setDisplayPage={setDisplayPage}/>
