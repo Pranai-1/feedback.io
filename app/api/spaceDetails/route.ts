81075ab28fd967a4e94e8066b34ce8e98d556241
Pranai-1 <reddypranai2017@gmail.com>
2024-12-23 20:24:33 +0530

    post api for space creation

--- app/api/spaceDetails/route.ts
+++ app/api/spaceDetails/route.ts
@@ -1,20 +1,32 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod"
+import { SpaceInputs } from "../types";
 
 const spaceSchema = z.object({
-  userId:z.string().min(1, "userId is required."),
+  userId:z.string().min(1, "Please login and try again."),
   spaceName: z.string().min(1, "Space name is required."),
   description:z.string().min(1, "Space description is required."),
   title: z.string().min(1, "Header title is required."),
   customMessage: z.string().min(1, "Custom message is required."),
   image: z.string().url("Invalid URL format for space logo."),
+  questions:z.string().array().min(3, { message: "At least three questions are required." }) 
 });
 
 
 export async function POST(req:NextRequest){
     const body=await req.json()
-    const { userId, spaceName, image, title, description, questions } = body;
+
+    try{
+        spaceSchema.safeParse(body)
+    }catch(error){
+         if (error instanceof z.ZodError) {
+            return NextResponse.json({message:"failed",error:error.errors[0].message})   
+        }
+        return NextResponse.json({message:"failed",error:"Error occured while creating space"})
+    }
+    
+    const { userId, spaceName, image, title, description, questions,customMessage } = body;
     try{
         const newSpace=await prisma.space.create({
             data: {
@@ -23,6 +35,7 @@ export async function POST(req:NextRequest){
                 image,
                 title,
                 description,
+                customMessage,
                 questions,
               },
         })
