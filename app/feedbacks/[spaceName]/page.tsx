5a83d4b91461ec8dd1afc72d5e3e587c891ea2ca
Pranai-1 <reddypranai2017@gmail.com>
2025-01-05 18:45:28 +0530

    removed console logs and unused variables

--- app/feedbacks/[spaceName]/page.tsx
+++ app/feedbacks/[spaceName]/page.tsx
@@ -3,33 +3,31 @@ import getWallOfLove from "@/app/actions/wallOfLoveActions/getWallOfLove";
 import { FeedbackPropType, WallOfLoveProp } from "@/app/api/types";
 import FeedbackHome from "@/app/components/Feedback/FeedbackHome";
 
-// interface FeedbackHomeProps {
-//   spaceName: string;
-// }
-
-export default async function SpaceFeedbacks({ params }: { params:  { spaceName: string } }) {
+export default function SpaceFeedbacks({ params }: { params: { spaceName: string } }) {
   const { spaceName } = params;
-  
-  let feedbacks: FeedbackPropType[] = [];
- let wallOfLove:WallOfLoveProp[]=[]
 
+  let feedbacks: FeedbackPropType[] = [];
+  let wallOfLove: WallOfLoveProp[] = [];
+
+  // Fetch reviews and WallOfLove data asynchronously
+  const fetchData = async () => {
     const reviews = await getFeedback(spaceName);
     if (reviews.success && Array.isArray(reviews.reviews)) {
-      feedbacks = reviews.reviews as FeedbackPropType[]; 
+      feedbacks = reviews.reviews as FeedbackPropType[];
     }
 
-    const liked=await getWallOfLove(spaceName)
+    const liked = await getWallOfLove(spaceName);
     if (liked.success && Array.isArray(liked.wallOfLove)) {
-      wallOfLove = liked.wallOfLove as WallOfLoveProp[]; 
+      wallOfLove = liked.wallOfLove as WallOfLoveProp[];
     }
+  };
+
+  // Call the async function to fetch the data
+  fetchData();
 
   return (
     <div className="relative h-full md:h-[86.8vh] lg:h-[87.2vh] xl:h-[87.5vh] w-[100%] bg-[#09090B] flex overflow-auto py-4 items-center md:justify-start justify-center">
-      <FeedbackHome 
-      feedbacks={feedbacks} 
-      spaceName={spaceName} 
-      wallOfLove={wallOfLove}
-      />
+      <FeedbackHome feedbacks={feedbacks} spaceName={spaceName} wallOfLove={wallOfLove} />
     </div>
   );
 }
