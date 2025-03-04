678ce97f76c679ed992f2daa905f39f98d25f6bf
Pranai-1 <reddypranai2017@gmail.com>
2025-03-04 14:38:03 +0530

    user profile

--- app/components/SideBar/UserProfile.tsx
+++ app/components/SideBar/UserProfile.tsx
@@ -2,6 +2,13 @@ import { fetchUserData } from "@/lib/dataFetch"
 import Image from "next/image"
 export default async function UserProfile(){
     const {user,spaces}=await fetchUserData()
+    let truncatedName=""
+    let truncatedEmail=""
+    if(user && user.name){
+        truncatedName=user.name.length >10 ? user.name.slice(0,12)+"..." : user.name
+        truncatedEmail=user.email.length >25 ? user.email.slice(0,25)+"..." : user.email
+    }
+  
     return(
         <div className="w-[100%] flex flex-col justify-center items-center gap-4">
             <div className="flex flex-col justify-center items-center w-[100%] gap-2">
@@ -14,8 +21,12 @@ export default async function UserProfile(){
                 </div>
 
             </div>
-         <div className="w-[90%] border border-gray-400 p-2 flex justify-center items-center gap-2">
-            <Image src={user?.image || ""} alt="profile" width={20} height={20}/>
+         <div className="w-[90%] border border-gray-400 p-2 flex justify-center items-center gap-2 rounded-md">
+            <Image src={user?.image || ""} alt="profile" width={50} height={20} className="rounded-full"/>
+            <div className="flex flex-col justify-center items-center gap-2 ">
+               <p className=" font-medium text-gray-200">{truncatedName}</p>
+               <p className=" text-gray-200 text-sm">{truncatedEmail}</p>
+            </div>
         </div>
         </div>
        
