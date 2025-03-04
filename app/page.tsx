f5c1c0c7f823569760d3dcde9c7155aa9d094f3f
Pranai-1 <reddypranai2017@gmail.com>
2025-03-04 19:25:06 +0530

    Home page completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@
 // import axios from "axios";
 // import { redirect } from "next/navigation";
 
+import HomeBody from "./components/HomePage/HomeBody";
 import Navbar from "./components/HomePage/Navbar";
-import { Particles } from "@/components/particles";
-import Header from "./components/HomePage/Header";
-import { ScrollProgress } from "@/components/scroll-progress";
+
 
 
 
@@ -17,15 +16,8 @@ export default async function Home() {
   <div className="bg-black">
      
     <Navbar/>
-  
-     <div className="w-[100%] h-screen bg-black relative">
-    
-     <Particles className="absolute z-80 h-full w-full"/> 
-     <ScrollProgress className="top-[85px] z-10 rounded-lg" />
-     <Header/>
-     
-    
-     </div>
+  <HomeBody/>
+   
   </div>
   );
 }
