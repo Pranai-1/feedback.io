ec9fe8535993a072b289dfbf921969b5f6341b25
Pranai-1 <reddypranai2017@gmail.com>
2025-03-04 19:52:11 +0530

    deployment check

--- app/components/HomePage/HomeBody.tsx
+++ app/components/HomePage/HomeBody.tsx
@@ -1,16 +1,16 @@
 
 
-"use client"
-import Header from "./Header";
-// import { ScrollProgress } from "@/components/scroll-progress";
+
+// import Header from "./Header";
+import { ScrollProgress } from "@/components/scroll-progress";
 
 export default function HomeBody(){
     return(
         <div className="w-[100%] h-screen bg-black relative">
     
         {/* <Particles className="absolute z-80 h-full w-full"/>  */}
-        {/* <ScrollProgress className="top-[85px] z-10 rounded-lg" /> */}
-        <Header/>
+        <ScrollProgress className="top-[85px] z-10 rounded-lg" />
+        {/* <Header/> */}
         
        
         </div>
