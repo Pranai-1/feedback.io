925542e3af179552c3bbf3506046e5c88d2de41b
Pranai-1 <reddypranai2017@gmail.com>
2025-03-06 16:28:23 +0530

    fixed slider card

--- app/components/HomePage/WidegtDisplay.tsx
+++ app/components/HomePage/WidegtDisplay.tsx
@@ -1,19 +1,19 @@
 "use client"
 
-// import { BlurFade } from "@/components/magicui/blur-fade";
-// import {FeedbackWidget} from "feedback.io-widget"
+import { BlurFade } from "@/components/magicui/blur-fade";
+import {FeedbackWidget} from "feedback.io-widget"
 export default function WidgetDisplay(){
-    //const time=0.5
+    const time=0.5
     return(
         <div className="w-[100%]">
-        <p>kjewnckew</p>
-        {/* <BlurFade
+        
+        <BlurFade
           delay={time * 1}
           inView
           className="w-[100%] h-[300px]  overflow-y-hidden"
         >
           <FeedbackWidget pageName="slider" spaceName="Feedback.io"/>
-          </BlurFade> */}
+          </BlurFade>
         </div>
     )
 }
